5ecd040c30af887cca2bbf51c580cd5c148f278b
dapindersingh-sudo <57323783+dapindersingh-sudo@users.noreply.github.com>
2019-11-17 18:09:36 +0530

    Add files via upload

--- hello.py
+++ hello.py
@@ -3,17 +3,22 @@ a_atn=input("Enter classes you have attended: ")
 tclas=float(t_atn)
 aclas=float(a_atn)
 prcnt=(aclas/tclas)*100
-pr=float(prcnt)
+# pr=float(prcnt)
 if prcnt == 75:
     print("You'r attenence is on borer")
     print("You can skip 0 class")
 elif prcnt>75:
     cnt=0
     print("You are above standard")
-    while prcnt>=75:
-        prcnt=(aclas/tclas)*100
+    while prcnt>75:
         tclas=tclas+1
+        prcnt=round((aclas/tclas)*100)
         cnt=cnt+1
-    print(cnt)
+    print(f'You can skip {cnt} classes')
 elif prcnt<75:
-    print("Please attend classes")
+    cnt=0
+    while prcnt<75:
+        aclas = aclas+1
+        prcnt=int((aclas/tclas)*100)
+        cnt=cnt+1
+    print(f'You have to attend {cnt} classes')
